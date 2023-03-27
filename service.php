3e628e64dfc76a625d6e4c3a24cc55c389a0592a
Koen de Wolf de Wolf <koendewolfmail@gmail.com>
2023-03-27 19:49:32 +0200

    Add warning regarding lambda and callable functions

--- service.php
+++ service.php
@@ -4,6 +4,9 @@
  * A place to define your global variables
  * These variables will be injected as properties in the contoller
  * A good use case would be putting the db connection variable here
+ * 
+ * These variables will only be accessible to methods defined in a controller
+ * If you want to access these variables use $GLOBALS['service'] to retrieve them
  */
 
 $globalVar = 'placeholder';
