87a5bd274a334dd091b25b20b1736745e02a6288
Koen de Wolf <koendewolfmail@gmail.com>
2023-10-18 16:39:18 +0200

    Move all imports to requires.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-require_once 'vendor/autoload.php';
-require_once 'settings.php';
-require_once 'commands.php';
-require_once 'routes.php';
-require_once 'dotenv.php';
-require_once 'service.php';
-require_once 'src/util/Functions.php';
+require_once 'requires.php';
 
 use src\app\CliApp;
 use src\app\HttpApp;
---
+++ requires.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'vendor/autoload.php';
+require_once 'settings.php';
+require_once 'commands.php';
+require_once 'routes.php';
+require_once 'dotenv.php';
+require_once 'service.php';
+require_once 'src/util/Functions.php';
