732faa45866236a0a0324f3850ac30c92fda0d3f
Koen de Wolf <koendewolfmail@gmail.com>
2024-01-04 23:52:37 +0100

    Create ClassImporter.php

---
+++ src/filesystem/ClassImporter.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace src\filesystem;
+
+use src\exceptions\FilesystemException;
+
+class ClassImporter
+{
+    public static function importAsString(string $baseDir, string $path, array $exclude = []): ?array
+    {
+        $absolutePath = static::appendToBaseDir($baseDir, $path);
+
+        if (!file_exists($absolutePath)) {
+            throw new FilesystemException(sprintf('file %s does not exist', $absolutePath));
+        }
+
+        $dirItems = array_filter(
+            scandir($absolutePath),
+            function (string $item) {
+                return (!in_array($item, ['.', '..']) && str_ends_with($item, '.php'));
+            }
+        );
+
+        $classNames = array_map(
+            function (string $item) use ($path) {
+                $namespacePath = str_replace(['/', '\\'], '\\', $path);
+                $className = explode('.', $item)[0];
+
+                return trim(
+                    sprintf('%s\\%s', $namespacePath, $className),
+                    '\\'
+                );
+            },
+            $dirItems
+        );
+
+        $filteredClassNames = array_filter(
+            $classNames,
+            function (string $className) use ($exclude) {
+                return !in_array($className, $exclude);
+            }
+        );
+
+        return array_values($filteredClassNames);
+    }
+
+    public static function importAsClass(string $baseDir, string $path, array $exclude = [], array $args = []): ?array
+    {
+        $classNames = static::importAsString($baseDir, $path, $exclude);
+
+        $classes = array_map(
+            function (string $className) use ($args) {
+                return new $className(...$args);
+            },
+            $classNames
+        );
+
+        return array_values($classes);
+    }
+
+    protected static function appendToBaseDir(string $baseDir, string $relativePath): string
+    {
+        $chars = ['/', '\\'];
+
+        foreach ($chars as $char) {
+            $baseDir = rtrim($baseDir, $char);
+            $relativePath = trim($relativePath, $char);
+        }
+
+        return sprintf(
+            '%s%s%s',
+            $baseDir,
+            DIRECTORY_SEPARATOR,
+            $relativePath
+        );
+    }
+}
