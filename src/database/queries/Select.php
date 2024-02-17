79a5a18f0579d3a2208934f094f65558137170ba
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2024-02-17 22:33:13 +0100

    Update Select.php

--- src/database/queries/Select.php
+++ src/database/queries/Select.php
@@ -5,7 +5,7 @@ namespace src\database\queries;
 use PDO;
 use PDOStatement;
 use src\exceptions\ModelException;
-use src\models\Model as DatabaseModel;
+use src\models\Model as CustomModel;
 
 trait Select
 {
@@ -40,7 +40,7 @@ trait Select
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function selectModel(): ?DatabaseModel
+    public function selectModel(): ?CustomModel
     {
         if (!$this->model) {
             throw new ModelException('no model supplied');
@@ -73,7 +73,7 @@ trait Select
         $assocs = $statement->fetchAll(PDO::FETCH_ASSOC);
 
         return array_map(
-            function (array $assoc) use ($statement): DatabaseModel {
+            function (array $assoc) use ($statement): CustomModel {
                 $model = new $this->model($this->database);
                 return $model->hydrateByAssoc($statement, $assoc);
             },
