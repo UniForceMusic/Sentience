35f4a74b54a3bd89b94e95aa9da2b18dab70eccf
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2024-02-19 18:33:20 +0100

    Add custom functions file

---
+++ src/util/CustomFunctions.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Add your own custom functions here.
+ * 
+ * Since Functions.php will be updated by newer versions of Sentience.
+ * All custom user functions can be placed in a separate file.
+ */
